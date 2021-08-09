75bc7fc138f73c55cde9f1b54c6b07c23679047f
Tom Luchesi <69174010+Tom-L-M@users.noreply.github.com>
2021-08-09 14:10:35 -0300

    Update cellium.js

--- cellium.js
+++ cellium.js
@@ -1,10 +1,14 @@
 "use strict"
-
-// This is a router, designed for handling new features in future updates.
-const Namespace = require('./namespace');
+// This is just a router, designed for handling new features in future updates.
+const Namespace = require('./src/namespace');
 const Cellium = { 
-    create: function (a) { return new Namespace(a); },
+    create: function () { return new Namespace(); },
     depth: function (a) { return Namespace.depth(a); },
     extend: function (a, b) { return Namespace.extend(a, b); },
+    isInstance: function (a) { return Namespace.isInstance(a); },
+    list: function (a, b) { return Namespace.list(a, b); },
+    flat: function (a) { return Namespace.flat(a); },
+    clone: function (a) { return Namespace.clone(a); },
+    merge: function (a, b) { return Namespace.merge(a, b); }
 }
 module.exports = Cellium;
